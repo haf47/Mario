af80b3535a247af9305d72f9daaa529d622ecaca
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-05 11:56:50 -0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,24 @@
+
+let base = 8;
+
+let torre = '';
+
+for(let i = base; i >= 1; i--){
+    for(let j = 1; j <= i; j++){
+        torre += '#';
+    }
+    torre += '\n';
+}
+
+console.log(torre);
+    
+
+
+
+
+
+
+
+    
+
+
